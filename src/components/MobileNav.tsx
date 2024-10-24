536b537c23c667d889500a3812073e4dd08c6afd
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-24 06:33:02 +0600

    mobile navbar created

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,3 +1,5 @@
+import { BASE_URL } from '@/utilityComponents/Const';
+import axios from 'axios';
 import { useParams, useSearchParams } from 'next/navigation';
 import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
@@ -13,11 +15,23 @@ const MobileNav = ({input,setInput,handleSubmit}:propsType) => {
     const searchParams = useSearchParams();
     const params = useParams();
     useEffect(()=>{
-        
+        axios.get(`${BASE_URL}/genre/movie/list?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US`)
+        .then(({data})=>{
+            setGenres((data.genres));
+        })
+        .catch(err=>console.log(err));
     },[])
+
+    useEffect(()=>{
+        if(searchParams.get("genre")){
+            setSelectedGenre(searchParams.get("genre")!)
+            return;
+        }
+        setSelectedGenre(params.id?.toString());
+    },[searchParams.get("genre"),params.id]);
     return (
         <div>
-            
+            <form className='md:hidden flex justify-between w-[100%]' onSubmit={handleSubmit}></form>
         </div>
     );
 };
---
+++ src/utilityComponents/Const.ts
@@ -0,0 +1,2 @@
+export const BASE_URL ="http://api.themoviedb.org/3";
+export const BASE_IMG_URL="http://image.tmdb.org/t/p/w500";
